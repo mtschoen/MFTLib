01a887076227f27ba801cf374404378f6bbb90bb
Matt Schoen <mtschoen@gmail.com>
2024-11-10 15:42:40 -0800

    Add project files.

---
+++ MFTLib/MFTParse.cs
@@ -0,0 +1,11 @@
+﻿namespace MFTLib
+{
+    public class MFTParse
+    {
+        public static Dictionary<string, FileNode> GetFileNodes()
+        {
+            var files = new Dictionary<string, FileNode>();
+            return files;
+        }
+    }
+}
---
+++ TestProgram/Program.cs
@@ -0,0 +1,7 @@
+﻿using MFTLib;
+
+var files = MFTParse.GetFileNodes();
+foreach (var kvp in files)
+{
+    Console.WriteLine(kvp.Key);
+}
